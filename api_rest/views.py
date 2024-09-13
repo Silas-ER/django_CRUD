f1b81930eb2465413ecd6024cff788d7be021b4b
Silas Eduardo <silasn1@live.com>
2024-09-13 12:43:18 -0300

    feat: criação dos novos métodos

--- api_rest/views.py
+++ api_rest/views.py
@@ -33,6 +33,8 @@ def get_by_nick(request, nick):
     
 @api_view(['GET','POST','PUT','DELETE'])
 def user_manager(request):
+
+    # OBTER DADOS
     if request.method == 'GET':
         try:
             if request.GET['user']:
@@ -52,4 +54,46 @@ def user_manager(request):
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST) #requisição veio com parametro errado
         
+    # CRIAR DADOS
+    if request.method == 'POST':
+        new_user = request.data
+
+        serializer = UserSerializer(data=new_user) #serializando dados 
+
+        if serializer.is_valid(): #verificação se os dados são validos
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    # MODIFICAR DADOS
+    if request.method == 'PUT':
+
+        nickname = request.data['user_nickname'] #parametro passado na url
+
+        try:
+            updated_user = User.objects.get(pk=nickname)    #verifica se o nickname existe na base de dados 
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        print(request.data)
+
+        serializer = UserSerializer(updated_user, data=request.data) #indica qual objeto vai ser editado
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    # DELETAR DADOS
+    if request.method == 'DELETE':
+    
+        try:
+            user_to_delete = User.objects.get(pk=request.data['user_nickname'])    #verifica se o nickname existe na base de dados 
+            user_to_delete.delete() # deletamos diretamente o objeto
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
 # Create your views here.
